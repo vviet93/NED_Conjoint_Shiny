f226dd929ad1156ed1ec27943239090c8448db87
Steven Denney <scdenney@users.noreply.github.com>
2022-02-07 14:22:14 +0100

    updates

--- app.R
+++ app.R
@@ -417,10 +417,4 @@ server <- function(input, output, session) {
 }
 
 # Run the application 
-##shinyApp(ui = ui, server = server)
-
-# update
-#library(rsconnect)
-#rsconnect::deployApp()
-#deployApp()
-
+shinyApp(ui = ui, server = server)
